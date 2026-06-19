100bb93c035a64aa73570aff45dce01cdc592f10
Richard Trotta <trottar@jlab.org>
2026-06-19 12:03:00 -0400

    2025 dataset DIS fit using quad

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ DATASET_MODE = "2025"
 # DIS_FIT_MODEL = "quad"
 # DIS_FIT_MODEL = "cubic"
 # DIS_FIT_MODEL = "all"
-DIS_FIT_MODEL = "cubic"
+DIS_FIT_MODEL = "quad"
 
 # Analysis scope variants:
 # ANALYSIS_SCOPE = "full"

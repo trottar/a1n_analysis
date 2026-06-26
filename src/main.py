e459080fbd62029855c5dadf1ba267000bf7941d
Richard Trotta <trottar@jlab.org>
2026-06-26 01:43:46 -0400

    Added all data sources | 1

--- src/main.py
+++ src/main.py
@@ -80,7 +80,7 @@ DIS_W_MIN = 2.0
 # DIS_FIT_MODEL = "quad"
 # DIS_FIT_MODEL = "cubic"
 # DIS_FIT_MODEL = "all"
-DIS_FIT_MODEL = "quad_alpha"
+DIS_FIT_MODEL = "cubic_alpha"
 
 # Analysis scope variants:
 # ANALYSIS_SCOPE = "full"

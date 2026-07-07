dcd2ed08a817fd3774c3a9bc89f72b9cb1ac1a25
Richard Trotta <trottar@jlab.org>
2026-07-06 22:04:42 -0400

    Adding kzero and minor plot updates | 4

--- src/main.py
+++ src/main.py
@@ -90,7 +90,7 @@ DIS_CURRENT_SOURCE = "dis_csv"
 # BW_K_CURVE_MODEL = "non-tune"
 # BW_K_CURVE_MODEL = "tune"
 # BW_K_CURVE_MODEL = "fixed_zero"
-BW_K_CURVE_MODEL = "fixed_zero"
+BW_K_CURVE_MODEL = "tune"
 
 # DIS fit model variants:
 # DIS_FIT_MODEL = "fullx"
@@ -100,7 +100,7 @@ BW_K_CURVE_MODEL = "fixed_zero"
 # DIS_FIT_MODEL = "quad"
 # DIS_FIT_MODEL = "cubic"
 # DIS_FIT_MODEL = "all"
-DIS_FIT_MODEL = "cubic"
+DIS_FIT_MODEL = "cubic_alpha"
 
 # Analysis scope variants:
 # ANALYSIS_SCOPE = "full"

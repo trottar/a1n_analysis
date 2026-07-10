9a878e93dce40ef84095070c1b3aa116a9866abd
Richard Trotta <trottar@jlab.org>
2026-07-10 14:37:37 -0400

    Running k-zero fits | 1

--- src/main.py
+++ src/main.py
@@ -101,7 +101,7 @@ BW_K_CURVE_MODEL = "fixed_zero"
 # DIS_FIT_MODEL = "quad"
 # DIS_FIT_MODEL = "cubic"
 # DIS_FIT_MODEL = "all"
-DIS_FIT_MODEL = "cubic"
+DIS_FIT_MODEL = "quad_alpha"
 
 # Analysis scope variants:
 # ANALYSIS_SCOPE = "full"
